ccfbe1c5f02bb46d756ec855f63a6f43a28ae4f4
John Westover <78867913+Jowest015@users.noreply.github.com>
2021-07-01 19:25:51 -0500

    initial start; set up server

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var PORT = 2340;
+
+app.get('/', (req, res) => {
+  res.send('Hello Dave....');
+});
+
+app.listen(PORT, () => {
+  console.log(`Connection established at http://localhost:${PORT}`);
+});
