e56dd2633efaca51b9f0ecb2b493afff7cc4a141
John Westover <78867913+Jowest015@users.noreply.github.com>
2021-07-03 13:02:14 -0500

    add pg-promise

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ var express = require('express');
 var app = express();
 var PORT = 2340;
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.send('Hello Dave....');
 });
 
 app.listen(PORT, () => {
   console.log(`Connection established at http://localhost:${PORT}`);
 });
+
