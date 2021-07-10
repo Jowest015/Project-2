231a86409298eaf8e9adbd62862ce5b5e2ec48d6
John Westover <78867913+Jowest015@users.noreply.github.com>
2021-07-10 10:27:46 -0500

    connect db to server

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 var express = require('express');
+var pgp = require('pg-promise')();
+
 var app = express();
 var PORT = 2340;
 
-app.get('/', (_req, res) => {
-  res.send('Hello Dave....');
+var dbSettings = {user:"postgres", password:"Mr6tBU?RSe",  database: "recipe"};
+var db = pgp(dbSettings);
+
+// console.log(db);
+
+app.get('/', async (_req, res, next) => {
+  try {
+    const results = await db.query(`SELECT * FROM recipe`);
+    res.send({results});
+  } catch (error) {
+    next(error);
+    res.send({
+      error,
+      msg: "db table error, in db instance"
+    })
+  }
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, ()=> {
   console.log(`Connection established at http://localhost:${PORT}`);
 });
-
